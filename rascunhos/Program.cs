16fb2f9adf19c13ef3c9a6c4d2546cb18161969c
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-06-29 16:37:46 -0300

    atribuindo datas

--- rascunhos/Program.cs
+++ rascunhos/Program.cs
@@ -1,41 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Atribuindo valores para do tipo string, object e dynamic");
+Console.WriteLine("Atribuindo valores de data");
 Console.WriteLine();
 //=============================== COMANDOS INTERESSANTES =======================================
-string nome = "curso C#";
-String titulo = "Curso C# Essencial";
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
 
-Console.WriteLine(nome);
-Console.WriteLine(titulo);
+//criar uma data especifica 
+DateTime dataesp = new DateTime(2023,02,18);
+Console.WriteLine(dataesp);
 
-string valor = "Isto é uma string";
+//definindo as horas 
+DateTime dataesph = new DateTime(2023,02,18,10,5,55);
+Console.WriteLine(dataesph);
 
-Console.Write(valor);
-
-Console.ReadLine();
-
-object nota = 10;
-object valor1 = 8.55f;
-object nome1 = "Maria";
-object ativa = true;
-object letra = "A";
-
-Console.WriteLine(nota);
-Console.WriteLine(valor1);
-Console.WriteLine(nome1);
-Console.WriteLine(ativa);
-Console.WriteLine(letra);
-
-dynamic nota1 = 11;
-dynamic valor2 = 3.55f;
-dynamic nome2 = "Mar";
-dynamic ativa1 = false;
-dynamic letra1 = "B";
-
-Console.WriteLine(nota1);
-Console.WriteLine(valor2);
-Console.WriteLine(nome2);
-Console.WriteLine(ativa1);
-Console.WriteLine(letra1);
-
-Console.ReadLine();
+Console.ReadKey();

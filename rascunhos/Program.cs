d1662a5520cfe290cef39ad058e28ca47d8326b6
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-06-29 14:20:34 -0300

    declarando variaveis e constantes

--- rascunhos/Program.cs
+++ rascunhos/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Declarando Variaveis Numericas");
+//=============================== COMANDOS INTERESSANTES =======================================
+Console.ReadKey(); //Matem o terinal aberto
+const byte valor = 123;
+sbyte valor2 = -123;
+int valor3 = -12345677;
+uint valor4 = 12345677;
+long valor5 = -123456778901;
+
+Console.WriteLine(valor);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+

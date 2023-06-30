7ce96ffb5136eb20e8e8dfa5c421df79815213b7
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-06-30 17:13:47 -0300

    Operações Aritmeticas

--- rascunhos/Program.cs
+++ rascunhos/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("## Entrada de Dados ##\n");
+Console.WriteLine("## Operadores Aritméticos ##\n");
 //=============================== COMANDOS INTERESSANTES =======================================
-Console.WriteLine("Informe o seu nome");
-string nome  = Console.ReadLine();
-Console.WriteLine("Informe a sua idade");
-int idade = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"\n O seu nome é {nome} e você tem {idade} anos");
+Console.WriteLine("Informe o valor de x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Informe o valor de y");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"\n Soma de x+y = {x+y}");
+Console.WriteLine($"\n Subtração de x-y = {x-y}");
+Console.WriteLine($"\n Multiplicação de x*y = {x*y}");
+Console.WriteLine($"\n Divisão Inteira de x/y = {x/y}");
+double divisaoderesto = (double) x/y;
+Console.WriteLine($"\n Divisão de x/y = {divisaoderesto}");
+Console.WriteLine($"\n Módulo da divisão de x%y = {x%y}");

9ad1117956a93db3331df473217f57339166b537
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-06-30 10:21:17 -0300

    Adicionando o projeto do primeiro exercicio

--- exerciciosfase1/obj/Debug/net7.0/exerciciosfase1.AssemblyInfo.cs
+++ exerciciosfase1/obj/Debug/net7.0/exerciciosfase1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("exercicios")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("exerciciosfase1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("exercicios")]
-[assembly: System.Reflection.AssemblyTitleAttribute("exercicios")]
+[assembly: System.Reflection.AssemblyProductAttribute("exerciciosfase1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("exerciciosfase1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.

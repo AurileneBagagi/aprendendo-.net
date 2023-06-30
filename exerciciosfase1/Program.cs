36ffb1065c3475b60c6b75e8030636fa7d621598
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-06-30 13:33:07 -0300

    Exercicio 1- parte pratica

--- exerciciosfase1/Program.cs
+++ exerciciosfase1/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine(" ============== Execicios Fase 1 - Fundamentos ============\n");
+// ------------------------------------------
+Console.WriteLine("\nQuestão 1");
+byte idade = 35;
+Console.WriteLine(idade);
+// ------------------------------------------
+Console.WriteLine("\nQuestão 2");
+string name = "Maria";
+Console.WriteLine(name);
+// ------------------------------------------
+Console.WriteLine("\nQuestão 3");
+float altura = 3.45f;
+Console.WriteLine(altura);
+// ------------------------------------------
+Console.WriteLine("\nQuestão 4");
+DateTime data = new DateTime(1999,09,04);
+Console.WriteLine(data.ToShortDateString());
+// ------------------------------------------
+Console.WriteLine("\nQuestão 5");;
+const byte ano = 12;
+Console.WriteLine(ano);
+// ------------------------------------------
+Console.WriteLine("\nQuestão 6");
+double? nota = null;
+Console.WriteLine(nota);
+nota = 7.80d;
+Console.WriteLine(nota);
+// ------------------------------------------
+Console.WriteLine("\nQuestão 13");
+int x = 77,  y = 66; 
+Console.WriteLine(x+y);

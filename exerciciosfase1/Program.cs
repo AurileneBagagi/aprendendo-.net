a678c0077b7fa81a9bd1b42dbfeb21b74a65ca6f
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-06-30 14:40:41 -0300

    Exercicio 1- correção pratica

--- exerciciosfase1/Program.cs
+++ exerciciosfase1/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(" ============== Execicios Fase 1 - Fundamentos ============\n");
+//OBC.: as variaveis com C no final indica o correto do exercicio
 // ------------------------------------------
 Console.WriteLine("\nQuestão 1");
 byte idade = 35;
 int idadeC = 35;
 Console.WriteLine(idade);
+Console.WriteLine(idadeC);
 // ------------------------------------------
 Console.WriteLine("\nQuestão 2");
 string name = "Maria";
@@ -14,6 +16,7 @@ Console.WriteLine("\nQuestão 3");
 float altura = 3.45f;
 double alturaC = 3.45;
 Console.WriteLine(altura);
+Console.WriteLine(alturaC);
 // ------------------------------------------
 Console.WriteLine("\nQuestão 4");
 DateTime data = new DateTime(1999,09,04);
@@ -23,6 +26,7 @@ Console.WriteLine("\nQuestão 5");;
 const byte ano = 12;
 const int anoC = 12;
 Console.WriteLine(ano);
+Console.WriteLine(anoC);
 // ------------------------------------------
 Console.WriteLine("\nQuestão 6");
 double? nota = null;
@@ -30,6 +34,7 @@ Console.WriteLine(nota);
 nota = 7.80d;
 double? notaC = 7.80;
 Console.WriteLine(nota);
+Console.WriteLine(notaC);
 // ------------------------------------------
 Console.WriteLine("\nQuestão 13");
 int x = 77,  y = 66; 

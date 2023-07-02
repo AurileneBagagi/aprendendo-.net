e68e3bd4bce0c9778a9d7f0827283427e08b8512
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-02 20:20:31 -0300

    uso de constantes

--- rascunhos/Program.cs
+++ rascunhos/Program.cs
@@ -1,14 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("## Operadores Aritméticos ##\n");
+Console.WriteLine("## Uso da Constatente ##\n");
 //=============================== COMANDOS INTERESSANTES =======================================
-Console.WriteLine("Informe o valor de x");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Informe o valor de y");
-int y = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"\n Raiz Quadrada de X = {Math.Sqrt(x)}");
-Console.WriteLine($"\n Potencia de X elevado a Y = {Math.Pow(x,y)}");
-Console.WriteLine($"\n Minimo entre X e Y = {Math.Min(x,y)}");
-Console.WriteLine($"\n Máximo entre X e Y = {Math.Max(x,y)}");
-Console.WriteLine($"\n Coseno de X = {Math.Cos(x)}");
-Console.WriteLine($"\n Seno de X = {Math.Sin(x)}");
-Console.WriteLine($"\n Exponencial de X = {Math.Exp(x)}");
+double raio, perimetro, area;
+const double PI = 3.14;
+Console.Write("Informe aqui o raio do Circulo:");
+raio = Convert.ToDouble(Console.ReadLine());
+
+perimetro = 2*PI*raio;
+area = PI*raio*raio;
+Console.WriteLine($"Perimetro = {perimetro}");
+Console.WriteLine($"area = {area}");
+
+

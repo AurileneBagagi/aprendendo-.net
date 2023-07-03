584282a6c3408852fde975b419144dd18d43c925
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-03 11:44:26 -0300

    Operador Ternario parte 2

--- rascunhos/Program.cs
+++ rascunhos/Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("## Operador Ternario ##\n");
 //=============================== COMANDOS INTERESSANTES =======================================
-Console.Write("Informe a temperatura: \n");
-var temp = Convert.ToDouble(Console.ReadLine());
+Console.Write("Informe o valor de x: \n");
+var x = Convert.ToDouble(Console.ReadLine());
 
-var resultado = temp >27? "Quente": "Normal";
-Console.WriteLine($" O tempo está {resultado}");
+Console.Write("Informe o valor de y: \n");
+var y = Convert.ToDouble(Console.ReadLine());
+
+
+var resultado = x >y? "x é maior que y": x<y? "x é menor que y": x==y? "x é igual a y": "sem resultado";
+Console.WriteLine($"{resultado}");
 
 

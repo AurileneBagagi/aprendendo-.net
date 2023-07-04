32a8d606ab0f4012fff93698487b3f69d131196f
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-04 12:47:24 -0300

    instrução de repetição While

--- instrucaoWhile/Program.cs
+++ instrucaoWhile/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=============== Instrução de Repetição While =================\n");
+
+int i = 1;
+while (i <=10)
+{
+    Console.WriteLine($"i={i}");
+    i++;        
+}
+Console.WriteLine("=============== Instrução de Repetição While =================\n");
+int m = 10;
+while (m > 0)
+{
+    Console.WriteLine($"m={m}");
+    m--;        
+}

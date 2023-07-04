c7f4d1127fa91e0f2dcf64f40111ead4ad3ca1cb
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-04 11:55:51 -0300

    instrução Switch 2

--- instrucaoSwitch/Program.cs
+++ instrucaoSwitch/Program.cs
@@ -1,38 +1,25 @@
 ﻿Console.WriteLine("=============== Instrução Switch =================");
-Console.WriteLine("Numero de parcelas");
+Console.WriteLine("Dias dos meses do ano");
 
-int compra = 600;
-Console.WriteLine($"Valor da compra R${compra}");
-Console.Write("Informe o numero de parcelas (1 a 3): ");
-var nParcelas = Convert.ToInt32(Console.ReadLine());
+Console.Write("Informe o nome do mês: ");
+var mes = Console.ReadLine().ToLower();
 
-switch (nParcelas)
+switch (mes)
 {
-    case 1:
-        Console.WriteLine($"\nPrestação R${compra/nParcelas}");
+    case "janeiro":
+    case "março":
+    case "maio":
+    case "julho":
+    case "agosto":
+    case "outubro":
+    case "dezembro":
+        Console.WriteLine("Este mês tem 31 dias");
         break;
-    case 2:
-        Console.WriteLine($"\nPrestação R${compra/nParcelas}");
-        break;
-    case 3:
-        Console.WriteLine($"\nPrestação R${compra/nParcelas}");
+    case "fevereiro":
+        Console.WriteLine("Este mês tem 28 ou 29 dias");
         break;
     default:
-        Console.WriteLine("Valor invalido, informe 1, 2, ou 3");
-        break;
+        Console.WriteLine("Este mês tem 30 dias");
+        break;      
 }
-
-Console.WriteLine("Impar ou Par");
-
-Console.Write("Informe o numero: ");
-var numero = Convert.ToInt32(Console.ReadLine());
-
-switch (numero%2)
-{
-    case 0:
-        Console.WriteLine($"\n {numero} é par");
-        break;
-    default:
-        Console.WriteLine($"\n {numero} é impar");
-        break;
-}
+Console.WriteLine("\nFim do processamento...");

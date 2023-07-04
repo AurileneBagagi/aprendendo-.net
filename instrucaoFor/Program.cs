b62fc5c64fbe6e59b3bedcbe2d28da370062e7df
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-04 15:41:11 -0300

    instrução de repetição For - Tabuada

--- instrucaoFor/Program.cs
+++ instrucaoFor/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=============== Instrução de Repetição For =================");
+
+int resultado, numero, i;
+Console.Write("Informe o número inteiro:");
+numero = Convert.ToInt32(Console.ReadLine());
+
+for (i = 0; i <= 10; i++) //caso queira incrementar +2 seria i+=2
+{
+    resultado = numero * i;
+    Console.WriteLine($"{numero}*{i}={resultado}");
+}
+
+Console.WriteLine("\nFim do processamento...");

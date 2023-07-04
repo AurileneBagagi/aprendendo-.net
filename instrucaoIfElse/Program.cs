f041de8e78ec5ad3d06398b13851240f52075967
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-04 11:14:58 -0300

    If-Else-If

--- instrucaoIfElse/Program.cs
+++ instrucaoIfElse/Program.cs
@@ -1,38 +1,24 @@
-﻿Console.WriteLine("=============== Instrução IF-Else =================");
+﻿Console.WriteLine("=============== Instrução IF-Else-IF =================");
 Console.WriteLine("Caso do Aluno aprovado ou reprovado");
 
-Console.Write("Informe a nota do aluno \t");
-var client = Convert.ToInt32(Console.ReadLine());
+Console.Write("Informe a nota do aluno: ");
+var nota = Convert.ToDouble(Console.ReadLine());
 
-if (client > 5)
+if(nota < 5)
+{
+    Console.WriteLine("Aluno Reprovado");
+}
+else if (nota >= 5 && nota <6)
+{
+    Console.WriteLine("Aluno em Recuperação");
+}
+else if (nota >= 6 && nota <=9)
 {
     Console.WriteLine("Aluno Aprovado");
 }
 else
 {
-    Console.WriteLine("Aluno Reprovado");
+    Console.WriteLine("Aluno Aprovado com distinção");
 }
 Console.WriteLine("Fim do processamento\n");
 
-Console.WriteLine("Caso do If-Else Aninhado");
-
-Console.Write("Informe o valor de x: \t");
-var x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Informe o valor de y: \t");
-var y = Convert.ToInt32(Console.ReadLine());
-if (x>y)
-{
-    Console.WriteLine("X é maior que y");
-}
-else
-{
-    if (x<y)
-    {
-        Console.WriteLine("X é menor que y");
-    }
-    else
-    {
-        Console.WriteLine("X é igual a y");
-    }
-}
-Console.WriteLine("Fim do processamento\n");

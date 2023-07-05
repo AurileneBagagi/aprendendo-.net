eb0efd7b53731cc1cd50bc6f88037593f5ead912
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-05 15:32:13 -0300

    comentarios sobre construtores

--- 5Construtores/Program.cs
+++ 5Construtores/Program.cs
@@ -2,6 +2,7 @@
 
 Aluno aluno = new Aluno("Maria",22,"feminino","S");
 
+//verificando os valores padrões ou posto ao intanciar um objeto.
 Console.WriteLine(aluno.Nome == null ? "null" : aluno.Nome);
 Console.WriteLine(aluno.Idade == 0 ? "zero" : aluno.Idade);
 Console.WriteLine(aluno.Sexo == null ? "null" : aluno.Sexo);
@@ -11,8 +12,9 @@ Console.ReadKey();
 
 public class Aluno
 {
-    public Aluno(string nome)=> Nome = nome;
-
+    //criando um construtor para apenas um parametro, para criar um construtor digitar ctor
+    public Aluno(string nome)=> Nome = nome;// corpo de expressão/expression body
+    //a palavra this chama outro construtor, como se fosse uma herança
     public Aluno(string nome,int idade, string sexo, string aprovado):this(nome)
     {
        Idade = idade;  

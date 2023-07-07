8605d436ccf11f1930501baf2d3ecbb2b15eb059
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-07 14:35:58 -0300

    Anotações sobre tratamento de erros

--- 20TratamentoErros/Program.cs
+++ 20TratamentoErros/Program.cs
@@ -12,12 +12,15 @@ try
     int z = x / y;
     Console.WriteLine($"\n{x} / {y} = {z}");
 }
-catch (Exception ex)
+catch (Exception exemplo) //exempro é uma variavel criada para informar o erro
 {
     Console.WriteLine("\nNão existe divisão por zero, tente outro número...");
 
-    Console.WriteLine($"\n Erro: <<< {ex.Message} >>>");
-    Console.WriteLine($"\n Detalhes : {ex?.StackTrace?.ToString()}");
+    Console.WriteLine($"\n Erro: <<< {exemplo.Message} >>>");
+    Console.WriteLine($"\n Detalhes : {exemplo?.StackTrace?.ToString()}");
+    //Operador condicional nulo: ex.:a?
+    //1. Se a for avaliado como nulo, o resultado de a?.x será null;
+    //2. Se a for avaliado como não nulo, o resultado de a?.x será igual ao resultado de a.x
 
 }
 finally

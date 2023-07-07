0db942867245a97b1e3679f5e49590eef6af4599
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-07 09:33:46 -0300

    Anotações sobre structs

--- 17Structs/Program.cs
+++ 17Structs/Program.cs
@@ -1,5 +1,10 @@
 ﻿Console.WriteLine("## Structs ##\n");
 
+// é ideal utilizar structs quando:
+ // 1: se as instancias do tipo forem pequenas e normalmente de curta duração
+ // 2: representa logicamente em um unico valor, semelhante ao tipos primitivos (int, double etc)
+ // 3: é imutavel
+ // 4: não precisa sofrer conversão para tipo de referencia (boxing) com frequência;
 Cliente cliente = new();
 cliente.Nome = "Maria";
 cliente.Idade = 19;

ecb3b99d2d295bdbe6e274dfcec65a86c569a42e
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-12 12:26:09 -0300

    comentarios sobre abstract

--- 11Heranca_abstract/Forma.cs
+++ 11Heranca_abstract/Forma.cs
@@ -7,9 +7,9 @@ public abstract class Forma
     public double Area { get; set; }    
     public double Perimetro { get; set; }
 
-    //métodos abstratos
-    public abstract void CalcularArea();
-    public abstract void CalcularPerimetro();
+    //métodos abstratos, têm que serem implementadas  pela classe devirada 
+    public abstract void CalcularArea(); //metodo para definir o valor da propriedade area
+    public abstract void CalcularPerimetro(); //metodo para definir o valor da propriedade perimetro
 
     //método comum
     public string Descricao()
--- 11Heranca_abstract/Quadrado.cs
+++ 11Heranca_abstract/Quadrado.cs
@@ -2,14 +2,15 @@
 
 public class Quadrado : Forma
 {
+    //A propriedades area e Perimentos são da classe abstratas
     public double Lado { get; set; }
     public override void CalcularArea()
-    {
+    { //metodo para definir o valor da propriedade area
         Area = Lado * Lado;
     }
 
     public override void CalcularPerimetro()
-    {
+    {//metodo para definir o valor da propriedade perimetro
         Perimetro = 4 * Lado;
     }
 }

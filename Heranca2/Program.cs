75f4a10bee83f23e2966c65c16122c5f71e8075e
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-14 08:32:21 -0300

    Exercicio Herança 2 - Algumas respostas

---
+++ Heranca2/Program.cs
@@ -0,0 +1,23 @@
+﻿A a = new A();
+B b = new B();
+A x;
+x = a;
+x.Exibir();
+x = b;
+x.Exibir(); 
+
+
+class B: A 
+{
+public override void Exibir() 
+{
+Console.WriteLine("B"); } 
+}
+
+class A
+{
+public virtual void Exibir() 
+{
+Console.WriteLine("A"); 
+} 
+}

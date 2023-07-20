ae85a9832ac5d99fae2fcf60b8bf4f2a218f9253
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-20 09:59:45 -0300

    |Consultas LINQ: Macoratti - Comentarios

--- 12LINQConsultas1/Program.cs
+++ 12LINQConsultas1/Program.cs
@@ -10,7 +10,7 @@ foreach(var produto in produtoEletronicos)
     Console.WriteLine($"{produto.Nome} \t{produto.Preco:C2}");
 }
 
-// Filtrando com duas condições
+// Filtrando com duas condições ----------------------------------------------------------------------------
 Console.WriteLine("\nProdutos mais caros com estoque superior a 5: ");
 var produtosCarosComEstoque = listaProdutos
                               .Where(p => p.Preco >= 2000 && p.Estoque > 5);
@@ -20,7 +20,7 @@ foreach (var produto in produtosCarosComEstoque)
     Console.WriteLine($"{produto.Nome}  \t{produto.Preco:C2}  Estoque: {produto.Estoque}");
 }
 
-// Filtrando com ordenação
+// Filtrando com ordenação  ----------------------------------------------------------------------------
 Console.WriteLine("\nLista de produtos com estoque minimo(<10) ordenados por nome");
 int minimo = 10;
 var produtosEstoqueMinimo = listaProdutos
@@ -32,7 +32,7 @@ foreach (var item in produtosEstoqueMinimo)
     Console.WriteLine($"{item.Nome} \t Estoque: {item.Estoque}");
 }
 
-// Ordenando por mais de um criterio
+// Ordenando por mais de um criterio  ----------------------------------------------------------------------------
 Console.WriteLine("\nProdutos ordenados por categoria e nome:");
 var produtosOrdenadosPorCategoriaENome = listaProdutos
                                          .OrderBy(p => p.Categoria)
@@ -47,7 +47,7 @@ foreach (var produto in produtosOrdenadosPorCategoriaENome)
     }
     Console.WriteLine($"        {produto.Nome}");
 }
-//Cria uma lista de strings com nomes dos produtos
+//Cria uma lista de strings com nomes dos produtos  ----------------------------------------------
 Console.WriteLine("\nLista dos nomes dos produtos ordenados");
 
 var nomesDosProdutos = listaProdutos.Select(p=> p.Nome).OrderBy(nome=>nome);
@@ -56,12 +56,12 @@ foreach (var produto in nomesDosProdutos)
     Console.WriteLine(produto);
 }
 
-// Filtrando por preço, ordenando por nome e criando um tipo anônimo
+// Filtrando por preço, ordenando por nome e criando um tipo anônimo  -----------------------------------------------
 Console.WriteLine("\nProdutos com valor menor que R$ 500 com aumento de 10% ordenados por nome:");
 var resultado = listaProdutos.Where(p => p.Preco < 500)
                              .OrderBy(p => p.Nome)
                              .Select(p => new
-                             {
+                             { //criando um tipo anonimo
                                  NomeProduto = p.Nome.ToUpper(),
                                  PrecoComAumento = p.Preco*1.1
                              });
@@ -71,7 +71,7 @@ foreach (var produto in resultado)
     Console.WriteLine($"{produto.NomeProduto}, \tPreço com aumento: {produto.PrecoComAumento:C}");
 }
 
-//outro exemplo
+//outro exemplo  ----------------------------------------------------------------------------
 Console.WriteLine("\nProdutos com valor maior que R$ 2000 com desconto de 20% ordenados por preço: ");
 var resultado1 = listaProdutos.Where(p => p.Preco > 2000)
                         .OrderBy(p => p.Preco)
@@ -85,7 +85,7 @@ foreach (var p in resultado1)
     Console.WriteLine($"{p.NomeDoProduto}, \tPreço com Desconto: {p.PrecoComDesconto:C}");
 }
 
-//Realizando cálculos de somatória, média e contagem
+//Realizando cálculos de somatória, média e contagem  ----------------------------------------------------
 Console.WriteLine("\nValor médio dos produtos eletrônicos");
 string categoria = "Eletrônicos";
 double mediaPrecoEletronicos = listaProdutos.Where(p => p.Categoria == categoria)
@@ -93,13 +93,13 @@ double mediaPrecoEletronicos = listaProdutos.Where(p => p.Categoria == categoria
 
 Console.WriteLine($"{mediaPrecoEletronicos:C}");
 
-
+// ----------------------------------------------------------------------------
 Console.WriteLine("\nValor total dos produtos em estoque");
 double valorTotalEstoque = listaProdutos
                            .Where(p => p.Estoque > 0)
                            .Sum(p => p.Preco * p.Estoque);
 Console.WriteLine($"{valorTotalEstoque:C}");
-
+// ----------------------------------------------------------------------------
 Console.WriteLine("\nQuantidade de produtos com estoque minimo (< 10)");
 int estoqueMinimo = 10;
 int produtosEstoqueBaixo = listaProdutos.Where(p => p.Estoque < estoqueMinimo).Count();
--- 14LINQAgrupamento1/Program.cs
+++ 14LINQAgrupamento1/Program.cs
@@ -1,13 +1,47 @@
 ﻿using _14LINQAgrupamento1;
 var listaProdutos = Produto.GetProdutos();
 
-var produtosPorCategoria = listaProdutos.GroupBy(p => p.Categoria);
+Console.WriteLine("------ Ordenando apenas as categorias ----------------\n");
+var produtosCategoria = listaProdutos.GroupBy(p => p.Categoria)
+                                        .OrderBy(c=>c.Key) //ordenando por categoria, que foi nomeado chave a cima
+                                        ;
+                                         
 
-foreach (var grupo in produtosPorCategoria)
+foreach (var grupo in produtosCategoria)
 {
-    Console.WriteLine(grupo.Key + " : " + grupo.Count());
+    Console.WriteLine(grupo.Key+ ":\t"+ grupo.Count());
 
     foreach (var produto in grupo)
+    {
+        Console.WriteLine($"  {produto.Nome} \t{produto.Preco:C2} \t{produto.Estoque}");
+    }
+}
+
+Console.WriteLine("\n------ Ordenando as categorias e os produtos ----------------\n");
+var produtosECategoria = listaProdutos.GroupBy(p => p.Categoria)
+                                        .OrderBy(c=>c.Key) //ordenando por categoria, que foi nomeado chave a cima
+                                        .Select(g=> new //selecionando os Produtos
+                                        {
+                                            Categoria = g.Key,
+                                            Produtos = g.OrderBy(p=>p.Nome).Select(p => new
+                                            {
+                                                p.Nome,
+                                                p.Preco,
+                                                p.Estoque
+                                                 
+                                            })
+
+                                        }
+
+                                        );
+                                         
+
+foreach (var grupo in produtosECategoria)
+{
+    //Console.WriteLine(grupo.Key+ ":\t"+ grupo.Count());
+    Console.WriteLine(grupo.Categoria+ ":\t"+ grupo.Produtos.Count());
+
+    foreach (var produto in grupo.Produtos)
     {
         Console.WriteLine($"  {produto.Nome} \t{produto.Preco:C2} \t{produto.Estoque}");
     }

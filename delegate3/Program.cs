a5755e5be2dc76eeb98f36860d2598de40af6b6c
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-20 15:32:38 -0300

    Exercicios de Delegates, Labda e LINQ - Respondidos e corrigidos

---
+++ delegate3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Questão 3 do Exercicio de Delegate,Lambda e LINQ");
+
+MensagemBoasVindas mensagem = MensagemPortuges;
+mensagem+=MensagemIngles;
+mensagem.Invoke();
+
+static void MensagemPortuges()
+{
+    Console.WriteLine("Bem-Vindo!");
+}
+static void MensagemIngles()
+{
+    Console.WriteLine("Welcome!");
+}
+
+delegate void MensagemBoasVindas();
---
+++ delegate7/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Questão 7 do Exercicio de Delegate,Lambda e LINQ");
+
+List<Planeta> planetas = new List<Planeta> 
+{ 
+    new Planeta("Mercúrio", 4879, 3.301e23), 
+    new Planeta("Vênus", 12104, 4.867e24), 
+    new Planeta("Terra", 12756, 5.972e24), 
+    new Planeta("Marte", 6792, 6.39e23), 
+    new Planeta("Júpiter", 142984, 1.898e27), 
+    new Planeta("Saturno", 120536, 5.683e26), 
+    new Planeta("Urano", 51118, 8.681e25), 
+    new Planeta("Netuno", 49528, 1.024e26) 
+};
+
+Predicate<Planeta> predicate = p => p.Diametro>100000;
+List<Planeta> planetas100k = UsandoPredicate(planetas,predicate);
+
+foreach (var item in planetas100k)
+{
+    Console.WriteLine(item.Nome);
+}
+
+static List<Planeta> UsandoPredicate(List<Planeta> planetas, Predicate<Planeta> predicate)
+{
+    var resulado = new List<Planeta>();
+    foreach (var planeta in planetas)
+    {
+        if (predicate(planeta))
+            resulado.Add(planeta);
+    }
+    return resulado;
+}
+
+
+public class Planeta
+{
+    public string Nome { get; set; }
+    public double Diametro { get; set; }
+    public double Massa { get; set; }
+
+    public Planeta(string nome, double diametro, double massa)
+    {
+Nome = nome;
+        Diametro = diametro;
+        Massa = massa;
+    }
+}
